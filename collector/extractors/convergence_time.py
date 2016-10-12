834bd18f9133f5121dd38da2fc58b9b74ca4521e
glospoto <gab@lospoto.it>
2016-10-12 18:53:25 +0200

    Added control plane convergence time for Docker environment

--- collector/extractors/convergence_time.py
+++ collector/extractors/convergence_time.py
@@ -1,15 +1,17 @@
+"""
+This class implements an extractor for measuring the convergence time of an alternative.
+"""
+
 from abc import ABCMeta, abstractmethod
 
 from scapy.all import *
+from scapy.contrib.bgp import BGPHeader
+from scapy.contrib.ospf import OSPF_Hdr
 from scapy.layers.inet import TCP
 from scapy.utils import rdpcap
 
 from collector.extractor import Extractor
 from utils.file import File
-
-"""
-This class implements an extractor for measuring the convergence time of an alternative.
-"""
 
 
 class ControlPlaneConvergenceTime(Extractor):
@@ -96,6 +98,92 @@ class MininetControlPlaneConvergenceTime(ControlPlaneConvergenceTime):
 			self.__class__.__name__, 'Calculating the time of the of the first sniffed packet.')
 		# Take the time of the first sniffed packet
 		time_first_packet = openflow_packets[0].time
+		self._log.debug(self.__class__.__name__, 'Calculating the convergence time.')
+		# Calculate the convergence time
+		convergence_time = time_last_packet - time_first_packet
+		self._log.debug(
+			self.__class__.__name__, 
+			'Starting to write the convergence time into extractor folder.')
+		# Write it into a file inside the extractor folder
+		output_file = File(self._extractor_folder, self._extractor_file_name)
+		output_file.write('Convergence time (seconds): %s' % str(convergence_time))
+		output_file.save()
+		self._log.info(self.__class__.__name__, 'All data has been successfully extracted.')
+		# Notify all observers
+		self.notify_all('extractor')
+
+	'''
+	Run the thread in which this extractor is in execution.
+	'''
+
+	def run(self):
+		self.extract_data()
+
+"""
+This class implements an extractor for measuring the convergence time of an alternative running on 
+Docker simulator. This extractor is based on a control plane messages collector. The convergence 
+time measure is based on timestamps reported in the sniffed packets.
+"""
+
+
+class DockerControlPlaneConvergenceTime(ControlPlaneConvergenceTime):
+	def __init__(self):
+		ControlPlaneConvergenceTime.__init__(self)
+		# The abspath to the extraction folder
+		self._extractor_folder = None
+		# The file name which contains extracted information
+		self._extractor_file_name = 'time.data'
+
+	def __repr__(self):
+		return self.__class__.__name__
+
+	'''
+	Set the simulation path in which save the extracted data.
+	'''
+
+	def set_simulation(self, simulation):
+		# Set the simulation
+		self._simulation = simulation
+		# The abspath to the extraction folder
+		self._extractor_folder = self._fs.join(
+			simulation.get_simulation_path(), self._extractor_folder_name)
+		# Create extraction folder on the file system
+		self._fs.make_dir(self._extractor_folder)
+
+	'''
+	Start the process of extracting data.
+	'''
+
+	def extract_data(self):
+		# First of all, sleep for 1 minute
+		self._log.info(self.__class__.__name__, 'Sleeping waiting for data to extract.')
+		time.sleep(480)
+		self._log.info(self.__class__.__name__, 'I woke up. I am starting to extract data.')
+		# Take pcap files
+		pcap_files = self._fs.list_dir(self._fs.get_tmp_folder())
+		# The list of all openflow packets in the sniffing.
+		packets = []
+		for pcap_file in pcap_files:
+			if pcap_file.startswith('br-') and pcap_file.endswith('.pcap'):
+				# Load the sniff
+				pkts = rdpcap(self._fs.get_tmp_folder() + '/' + pcap_file)
+				self._log.debug(
+					self.__class__.__name__, 
+					'Calculating convergence time.')
+				for pkt in pkts:
+					p = pkt.payload
+					if BGPHeader in p or OSPF_Hdr in p:
+						packets.append(pkt.time)
+				# Delete the pcap file
+				self._fs.delete(self._fs.get_tmp_folder() + '/' + pcap_file) 
+		self._log.debug(
+			self.__class__.__name__, 'Calculating the time of the of the last sniffed packet.')
+		# Take the time of the last sniffed packet
+		time_last_packet = max(packets)
+		self._log.debug(
+			self.__class__.__name__, 'Calculating the time of the of the first sniffed packet.')
+		# Take the time of the first sniffed packet
+		time_first_packet = min(packets)
 		self._log.debug(self.__class__.__name__, 'Calculating the convergence time.')
 		# Calculate the convergence time
 		convergence_time = time_last_packet - time_first_packet
